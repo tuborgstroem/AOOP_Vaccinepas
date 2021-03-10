201ce3a53bc04f208736286a39085eeb9222e297
Flotfyr27 <42938246+Flotfyr27@users.noreply.github.com>
2021-03-10 09:41:44 +0100

    Added project template

---
+++ AOOP_Vaccine_Pas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AOOP_Vaccine_Pas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
