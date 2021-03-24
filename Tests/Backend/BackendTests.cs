076e59a4e5d8e84fb4bcbf56b0bf5aafb505ca4f
tuborgstroem <tborgstroem@gmail.com>
2021-03-24 14:29:14 +0100

    changed gitignore

--- Tests/Backend/BackendTests.cs
+++ Tests/Backend/BackendTests.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using BackendCore.Backend.Backend.cs;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace AOOP_Vaccine_Pas.Backend.Tests
 {
